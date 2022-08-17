53543432ea8ce6402a4b5d0b436e391594630436
Bilal Shakeel <98284706+mbilalshakeel@users.noreply.github.com>
2022-08-17 23:05:36 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/quizbrain.dart';
+
+void main() {
+  runApp(const Quizzler());
+}
+
+class Quizzler extends StatefulWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  State<Quizzler> createState() => _QuizzlerState();
+}
+
+class _QuizzlerState extends State<Quizzler> {
+  QuizBrain quiz=QuizBrain();
+  List<Icon> scorekeeper = [];
+
+  void onpress(bool user)
+  {
+    if(quiz.checkanswer(user))
+    {
+      scorekeeper.add(const Icon(Icons.check,color: Colors.green,));
+    }
+    else
+    {
+      scorekeeper.add(const Icon(Icons.close,color: Colors.red));
+    }
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: SizedBox(
+                    height: 450,
+                    width: 300,
+                    child: Center(child: Text(quiz.question(),style: const TextStyle(color: Colors.white,fontSize: 20))),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextButton(
+
+                child: Container(
+                  height: 60,
+                  width: 300,
+                  color: Colors.green,
+                  child: const Center(child: Text('True',style: TextStyle(color: Colors.white,fontSize: 25),)),
+                ),
+                onPressed: () {
+                  onpress(true);
+                   setState(() {
+                     quiz.nextquestion();
+                   });
+                },
+              ),
+              TextButton(
+
+                child: Container(
+                  height: 60,
+                  width:300,
+                  color: Colors.red,
+                  child: const Center(child: Text('False',style: TextStyle(color: Colors.white,fontSize: 25),)),
+                ),
+                onPressed: () {
+                  onpress(false);
+                  setState(() {
+                    quiz.nextquestion();
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: scorekeeper,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
